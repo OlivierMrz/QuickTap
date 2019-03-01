b7b6aadc19ef399dc21e4bc0ddae6343abb3c936
Olivier Miserez ( IK ) <olivier.miserez@me.com>
2019-03-01 20:12:17 +0100

    Initial Commit

---
+++ QuickTap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickTap
+//
+//  Created by Olivier school on 01/03/2019.
+//  Copyright © 2019 Olivier Miserez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
